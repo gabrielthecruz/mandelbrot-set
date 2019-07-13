b33775c071bf70fb485db5bcd1e41b3639005da2
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2019-07-13 03:52:43 -0300

    Import and export of points and performance improved

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,23 +2,8 @@ from matplotlib import colors
 import matplotlib.pyplot as plt
 
 import numpy as np
+import pandas as pd 
 import matplotlib
-
-
-def check_point(c, iterations=10):
-    r = 0
-    results = set()
-    for i in range(iterations):
-        r = r ** 2 + c
-
-        if r.real > 2 or r.imag > 2:
-            return False
-        elif r in results:
-            break
-
-        results.add(r)
-    
-    return True
 
 
 def check(c_number, iterations):
@@ -40,16 +25,49 @@ def check(c_number, iterations):
 def points(min_x, max_x, min_y, max_y):
     for x in np.arange(min_x, max_x, 0.001):
         for y in np.arange(min_y, max_y, 0.001):
-            yield x + (y * 1j)
+            yield round(x, 3) + (round(y, 3) * 1j)
 
 
-fig, ax = plt.subplots()
-colors = ['k'] * 5 + ['y'] * 5 + ['r'] * 5 + ['b'] * 5
-colors = 'kyrbb'
-for c_number in points(-2, 0.5, -1, 1):
-    is_valid, stop_iter = check(c_number, 5)
-    color = 'k' if is_valid else colors[stop_iter]
+def export_points():
+    import csv
 
-    ax.plot(c_number.real, c_number.imag, color + ',-')
+    x = (-2, 0.5)
+    y = (-1, 1)
+    i = 20
+    colors = 'yyyyyooooorrrrrbbbbb'
 
-plt.show()
+    with open('points_bkp.csv', 'w', encoding='utf-8', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter='\t')
+        csv_writer.writerow(('x', 'y', 'color'))
+
+        for coord in points(x[0], x[1], y[0], y[1]):
+            is_valid, stop_iter = check(coord, i)
+            color = 'k' if is_valid else colors[stop_iter]
+            csv_writer.writerow(coord.real, coord.imag, color)
+
+
+def read_points(ax):
+    import pandas as pd
+
+    points = pd.read_csv('points_bkp.csv', sep='\t')
+    colors = points['color'].unique()
+
+    for color in colors:
+        f_points = points[points.color == color]
+        ax.plot(f_points['x'], f_points['y'], ',-', color=color)
+
+    return ax
+
+
+def main():
+    fig, ax = plt.subplots()
+    
+    ax.set_xticks([-2, -1, 0, 1])
+    ax.set_yticks([-1, 0, 1])
+
+    ax = read_points(ax)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
