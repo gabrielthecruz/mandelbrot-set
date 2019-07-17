33593ff8337078ada96f026f0a88b0d19ffb0aaa
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2019-07-17 03:44:02 -0300

    Using more numpy
    Using more numpy for better performance

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,31 +1,29 @@
-from matplotlib import colors
-import matplotlib.pyplot as plt
-
-import numpy as np
-import pandas as pd 
 import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
 
 
-def check(c_number, iterations):
-    results = [0]
+def np_check(c_number, iterations):
+    rs = np.zeros(iterations, dtype=complex)
 
-    for i in range(iterations):
-        result = results[-1] ** 2 + c_number
+    for i in range(1, iterations):
+        rs[i] = rs[i-1] ** 2 + c_number
 
-        if result.real > 2 or result.imag > 2:
-            return False, i
-        elif result in results:
+        if rs[i].real > 2 and rs.imag > 2:
             break
-        
-        results.append(result)
+        elif rs[i] in rs[:i]:
+            break
     
-    return True, None
+    return rs[:i+1]
 
 
 def points(min_x, max_x, min_y, max_y):
-    for x in np.arange(min_x, max_x, 0.001):
-        for y in np.arange(min_y, max_y, 0.001):
-            yield round(x, 3) + (round(y, 3) * 1j)
+    xs = np.arange(min_x, max_x, 0.001).round(decimals=3)
+    ys = np.arange(min_y, max_y, 0.001).round(decimals=3)
+    cs = xs + (1j * ys)
+    
+    return cs
 
 
 def export_points():
@@ -33,38 +31,33 @@ def export_points():
 
     x = (-2, 0.5)
     y = (-1, 1)
-    i = 20
-    colors = 'yyyyyooooorrrrrbbbbb'
+    i = 50
 
-    with open('points_bkp.csv', 'w', encoding='utf-8', newline='') as csv_file:
+    
+    with open('points_bkp\points3.csv', 'w', encoding='utf-8', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter='\t')
-        csv_writer.writerow(('x', 'y', 'color'))
+        csv_writer.writerow(('x', 'y', 'mandelbrot?', 'iteração', 'z(n)'))
 
         for coord in points(x[0], x[1], y[0], y[1]):
-            is_valid, stop_iter = check(coord, i)
-            color = 'k' if is_valid else colors[stop_iter]
-            csv_writer.writerow(coord.real, coord.imag, color)
+            is_valid, stop_iter, zn = check(coord, i)
+            
+            csv_writer.writerow((coord.real, coord.imag, int(is_valid), stop_iter, zn))
 
 
 def read_points(ax):
-    import pandas as pd
-
-    points = pd.read_csv('points_bkp.csv', sep='\t')
-    colors = points['color'].unique()
-
-    for color in colors:
-        f_points = points[points.color == color]
-        ax.plot(f_points['x'], f_points['y'], ',-', color=color)
-
-    return ax
+    filename = 'points_bkp\points3.csv'
+    points = np.genfromtxt(filename, delimiter='\t', skip_header=1, 
+        dtype=(float, float, int), usecols=(0, 1, 2), names=('a', 'b', 'c'))
+    
+    xs = np.where(points['c'] == 1, points['a'], None)
+    ys = np.where(points['c'] == 1, points['b'], None)
+    ax.plot(xs, ys, ',-', color='black')
 
 
 def main():
+    # complexes = points(-2, 0.5, -1, 1):
     fig, ax = plt.subplots()
     
-    ax.set_xticks([-2, -1, 0, 1])
-    ax.set_yticks([-1, 0, 1])
-
     ax = read_points(ax)
     plt.show()
 
